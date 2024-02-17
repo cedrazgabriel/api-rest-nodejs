3d9c6652e1da49fdfe70f2275ce9c8a5b7ecb185
Gabriel Cedraz <cedrazdev@gmail.com>
2024-02-16 21:57:18 -0300

    feat: separate routes folder

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,23 +1,18 @@
-import { error } from 'console'
 import 'dotenv/config'
-import {z} from 'zod'
+import { z } from 'zod'
 
 const envSchema = z.object({
-    NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
-    DATABASE_URL: z.string(),
-    PORT: z.number().default(3333)    
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_URL: z.string(),
+  PORT: z.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
 
-if(_env.success === false) {
-    console.error('Invalid enviroment variables!', _env.error.format())
+if (_env.success === false) {
+  console.error('Invalid enviroment variables!', _env.error.format())
 
-    throw new Error('Invalid enviroment variables')
+  throw new Error('Invalid enviroment variables')
 }
 
 export const env = _env.data
-
-
-
-
